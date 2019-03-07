49b7d105bbddb6b99308e7dbf2473bc788a60e80
Soha Jin <soha@lohu.info>
2019-03-07 21:44:57 +0800

    [WIP] CAS

--- vj4/handler/ojc_connect.py
+++ vj4/handler/ojc_connect.py
@@ -1,9 +1,11 @@
-from aiohttp import web_exceptions
 from aioauth_client import OAuth2Client
+import aiohttp
 import datetime
 import asyncio
 import random
 import string
+import yarl
+import xmltodict
 
 from vj4 import app
 from vj4 import error
@@ -51,7 +53,7 @@ class ConnectUnifiedAuthHandler(base.Handler):
       await client.get_access_token(self.request.query)
       _, uniAuthUserReturn = await client.user_info()
       ojcUser = uniAuthUserReturn['data']
-    except web_exceptions.HTTPBadRequest as e:
+    except aiohttp.web_exceptions.HTTPBadRequest as e:
       raise error.ThirdPartyConnectError(client.name, e)
 
     udoc = await user.get_by_ojcId(ojcUser['schoolId'])
@@ -69,3 +71,45 @@ class ConnectUnifiedAuthHandler(base.Handler):
 
     self.json_or_redirect(self.reverse_url('domain_main'))
 
+@app.route('/ojc/connect/wzu', 'ojc_connect_wzu', global_route=True)
+class ConnectWzuPortalHandler(base.Handler):
+  async def get(self):
+    auth_url = yarl.URL.build(
+      scheme='http',
+      host='rz.wzu.edu.cn',
+      path='/zfca/login',
+      port=80,
+      query={
+        'service': '{}/ojc/connect/wzu'.format(options.url_prefix)
+      }
+    )
+    if 'ticket' not in self.request.query:
+      self.redirect(str(auth_url))
+      return
+
+    try:
+      auth_url.with_path('/zfca/serviceValidate')
+      auth_url.update_query({
+        'ticket': self.request.query.get('ticket')
+      })
+      async with aiohttp.ClientSession() as session:
+        async with session.get(str(auth_url)) as resp:
+          doc = xmltodict.parse(resp.text())
+          raise error.ThirdPartyConnectError('wzu_cas', doc)
+    except Exception as e:
+      raise error.ThirdPartyConnectError('wzu_cas', e)
+
+    # udoc = await user.get_by_ojcId(ojcUser['schoolId'])
+    # if udoc:
+    #   await asyncio.gather(user.set_by_uid(udoc['_id'],
+    #                                        loginat=datetime.datetime.utcnow(),
+    #                                        loginip=self.remote_ip),
+    #                        self.update_session(uid=udoc['_id']))
+    # else:
+    #   uid = int(ojcUser['schoolId']) if ojcUser['schoolId'].isnumeric() else await system.inc_user_counter()
+    #   password = random_string(16)
+    #   await user.add(uid, ojcUser['username'], password, '{}@me.iojc.cn'.format(ojcUser['schoolId']), self.remote_ip)
+    #   await user.set_by_uid(uid, ojcId=ojcUser['schoolId'])
+    #   await self.update_session(new_saved=False, uid=uid)
+    #
+    # self.json_or_redirect(self.reverse_url('domain_main'))
