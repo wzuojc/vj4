54a89f6bf4d62df542c6d6a2e9c26362aca208c6
Soha Jin <soha@lohu.info>
2019-03-13 02:04:35 +0800

    Random domain id

---
+++ vj4/ui/pages/home_domain_create.page.js
@@ -0,0 +1,9 @@
+import { NamedPage } from 'vj/misc/PageLoader';
+
+const page = new NamedPage('home_domain_create', async () => {
+  $(document).on('click', '#home-domain-create--generate-id', (ev) => {
+    $('.section__body form [name="id"]').val(`r${1003 + Math.floor(Math.random() * 199810130309) % 260817}`);
+  });
+});
+
+export default page;
